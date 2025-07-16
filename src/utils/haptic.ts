fa49b3f35982f45fc0b0607d624fa0a1db15aaa7
xuanzhi33 <xuanzhi33@qq.com>
2025-07-16 15:00:07 +0000

    feat: add haptic feedback utility for iOS and Android

---
+++ src/utils/haptic.ts
@@ -0,0 +1,22 @@
+import { impactAsync, AndroidHaptics, performAndroidHapticsAsync, ImpactFeedbackStyle } from 'expo-haptics'
+import { Platform } from 'react-native'
+
+export function haptic(type: ImpactFeedbackStyle = ImpactFeedbackStyle.Medium): void {
+  if (Platform.OS === 'ios') {
+    impactAsync(type)
+  } else if (Platform.OS === 'android') {
+    if (type === ImpactFeedbackStyle.Light) {
+      performAndroidHapticsAsync(AndroidHaptics.Keyboard_Tap)
+    } else if (type === ImpactFeedbackStyle.Medium) {
+      performAndroidHapticsAsync(AndroidHaptics.Confirm)
+    } else if (type === ImpactFeedbackStyle.Heavy) {
+      performAndroidHapticsAsync(AndroidHaptics.Reject)
+    } else if (type === ImpactFeedbackStyle.Rigid) {
+      performAndroidHapticsAsync(AndroidHaptics.Long_Press)
+    } else if (type === ImpactFeedbackStyle.Soft) {
+      performAndroidHapticsAsync(AndroidHaptics.Virtual_Key_Release)
+    } else {
+      performAndroidHapticsAsync(AndroidHaptics.Virtual_Key)
+    }
+  }
+}
