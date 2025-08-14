57a3efd921e8334374cba6572db03c9f8268b0a1
kangfenmao <kangfenmao@qq.com>
2025-08-14 21:06:17 +0800

    refactor: replace AppNavigator with MainStackNavigator, update navigation logic, and enhance HomeScreen with drawer navigation

--- src/types/naviagate.ts
+++ src/types/naviagate.ts
@@ -2,7 +2,7 @@ import { StackNavigationProp } from '@react-navigation/stack'
 
 export type RootStackParamList = {
   WelcomeScreen: undefined
-  HomeScreen: undefined
+  HomeScreen: { screen: string; params: { topicId: string } }
   ChatScreen: { topicId: string }
   SettingsScreen: undefined
   PersonalScreen: undefined
