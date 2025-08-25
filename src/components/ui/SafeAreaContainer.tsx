d2eb84ba0ef4141edbf16dea4e6ee457d8f8a318
eeee0717 <chentao020717Work@outlook.com>
2025-08-25 15:38:19 +0800

    chore

--- src/components/ui/SafeAreaContainer.tsx
+++ src/components/ui/SafeAreaContainer.tsx
@@ -1,7 +1,5 @@
 import { Theme, View, ViewProps } from '@tamagui/core'
-import * as NavigationBar from 'expo-navigation-bar'
-import * as StatusBar from 'expo-status-bar'
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Platform } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
@@ -14,15 +12,6 @@ interface SafeAreaContainerProps extends ViewProps {
 const SafeAreaContainer: React.FC<SafeAreaContainerProps> = ({ children, style, ...rest }) => {
   const insets = useSafeAreaInsets()
   const isDark = useIsDark()
-  const backgroundColor = isDark ? '#121213ff' : '#f7f7f7ff'
-
-  useEffect(() => {
-    if (Platform.OS === 'android') {
-      // Set the navigation bar style
-      NavigationBar.setBackgroundColorAsync(backgroundColor)
-      StatusBar.setStatusBarBackgroundColor(backgroundColor)
-    }
-  }, [backgroundColor])
 
   return (
     <Theme name={isDark ? 'dark' : 'light'}>
@@ -34,7 +23,7 @@ const SafeAreaContainer: React.FC<SafeAreaContainerProps> = ({ children, style, 
             paddingLeft: insets.left,
             paddingRight: insets.right,
             flex: 1,
-            backgroundColor
+            backgroundColor: isDark ? '#121213ff' : '#f7f7f7ff'
           },
           style
         ]}
