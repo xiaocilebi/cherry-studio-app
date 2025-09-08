2aa8be387ea1d566c05cd3e45dd3faaf3450cc2b
eeee0717 <chentao020717Work@outlook.com>
2025-09-08 16:00:20 +0800

    fix: AssistantItem and TextSelectionSheet style

--- src/components/assistant/AssistantItemCard.tsx
+++ src/components/assistant/AssistantItemCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Dimensions } from 'react-native'
-import { Text, View, XStack, YStack } from 'tamagui'
+import { Button, Text, XStack, YStack } from 'tamagui'
 
 import { useTheme } from '@/hooks/useTheme'
 import { Assistant } from '@/types/assistant'
+import { haptic } from '@/utils/haptic'
 
 import EmojiAvatar from './EmojiAvator'
 import GroupTag from './market/GroupTag'
@@ -24,49 +25,55 @@ const AssistantItemCard = ({ assistant, onAssistantPress }: AssistantItemCardPro
   const cardHeight = cardWidth / 0.78 // 宽高比为0.78
 
   const handlePress = () => {
+    haptic()
     onAssistantPress(assistant)
   }
 
   return (
-    <View
-      backgroundColor="$uiCardBackground"
-      padding={14}
+    <Button
+      onPress={handlePress}
       width={cardWidth}
       height={cardHeight}
+      padding={0}
+      backgroundColor="$uiCardBackground"
       borderRadius={16}
-      pressStyle={{ backgroundColor: '$gray20' }}
-      onPress={handlePress}>
-      <YStack gap={8} alignItems="center" height="100%">
-        <EmojiAvatar
-          emoji={assistant.emoji}
-          size={cardWidth / 2}
-          borderWidth={5}
-          borderColor={isDark ? '#333333' : '$backgroundPrimary'}
-        />
-        <Text fontSize={16} textAlign="center" numberOfLines={1} ellipsizeMode="tail">
-          {assistant.name}
-        </Text>
-        <YStack flex={1} justifyContent="space-between" alignItems="center">
-          <Text color="$textSecondary" fontSize={12} lineHeight={14} numberOfLines={3} ellipsizeMode="tail">
-            {assistant.description}
+      pressStyle={{
+        backgroundColor: '$gray20'
+      }}
+      unstyled>
+      <YStack padding={14} width="100%" height="100%" borderRadius={16}>
+        <YStack gap={8} alignItems="center" height="100%">
+          <EmojiAvatar
+            emoji={assistant.emoji}
+            size={cardWidth / 2}
+            borderWidth={5}
+            borderColor={isDark ? '#333333' : '$backgroundPrimary'}
+          />
+          <Text fontSize={16} textAlign="center" numberOfLines={1} ellipsizeMode="tail">
+            {assistant.name}
           </Text>
-          <XStack gap={10}>
-            {assistant.group &&
-              assistant.group.map((group, index) => (
-                <GroupTag
-                  key={index}
-                  group={group}
-                  fontSize={8}
-                  backgroundColor="$green10"
-                  color="$green100"
-                  borderWidth={0.5}
-                  borderColor="$green20"
-                />
-              ))}
-          </XStack>
+          <YStack flex={1} justifyContent="space-between" alignItems="center">
+            <Text color="$textSecondary" fontSize={12} lineHeight={14} numberOfLines={3} ellipsizeMode="tail">
+              {assistant.description}
+            </Text>
+            <XStack gap={10}>
+              {assistant.group &&
+                assistant.group.map((group, index) => (
+                  <GroupTag
+                    key={index}
+                    group={group}
+                    fontSize={8}
+                    backgroundColor="$green10"
+                    color="$green100"
+                    borderWidth={0.5}
+                    borderColor="$green20"
+                  />
+                ))}
+            </XStack>
+          </YStack>
         </YStack>
       </YStack>
-    </View>
+    </Button>
   )
 }
 
