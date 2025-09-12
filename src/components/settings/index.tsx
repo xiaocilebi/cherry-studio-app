eefc3075de92ee933668bac032cc437ecc6847b0
eeee0717 <chentao020717Work@outlook.com>
2025-09-12 14:35:25 +0800

    fix: refresh when edit provider icon

--- src/components/settings/index.tsx
+++ src/components/settings/index.tsx
@@ -64,7 +64,7 @@ export const PressableSettingRow = styled(XStack, {
   paddingLeft: 16,
   paddingRight: 16,
   backgroundColor: '$uiCardBackground',
-  pressStyle: { backgroundColor: '$gray20' }
+  pressStyle: { opacity: 0.5 }
 })
 
 export const SettingRowTitle = styled(Text, {
