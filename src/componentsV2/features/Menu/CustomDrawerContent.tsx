64c6c71e62a6379143297c6f83179ad49a5ebb19
eeee0717 <chentao020717Work@outlook.com>
2025-09-21 16:06:28 +0800

    fix: android drawer error

--- src/componentsV2/features/Menu/CustomDrawerContent.tsx
+++ src/componentsV2/features/Menu/CustomDrawerContent.tsx
@@ -8,7 +8,6 @@ import { useTopics } from '@/hooks/useTopic'
 import { haptic } from '@/utils/haptic'
 
 import { MarketIcon, Settings, UnionIcon } from '@/componentsV2/icons'
-import { SafeAreaContainer } from '@/componentsV2'
 import { Divider } from 'heroui-native'
 import { MenuTabContent } from './MenuTabContent'
 import YStack from '@/componentsV2/layout/YStack'
@@ -19,10 +18,13 @@ import { TopicList } from '../TopicList'
 import Image from '@/componentsV2/base/Image'
 import PressableRow from '@/componentsV2/layout/PressableRow'
 import { IconButton } from '@/componentsV2/base/IconButton'
+import { View } from 'react-native'
+import { useSafeArea } from '@/hooks/useSafeArea'
 
 export default function CustomDrawerContent(props: DrawerContentComponentProps) {
   const { t } = useTranslation()
   const { avatar, userName } = useSettings()
+  const insets = useSafeArea()
 
   const { topics } = useTopics()
 
@@ -57,56 +59,60 @@ export default function CustomDrawerContent(props: DrawerContentComponentProps) 
   }
 
   return (
-    <SafeAreaContainer>
-      <YStack className="gap-2.5 flex-1">
-        <YStack className="gap-1.5 px-2.5">
-          <PressableRow
-            className="flex-row justify-between items-center py-2.5 px-2.5 rounded-lg"
-            onPress={handleNavigateAssistantMarketScreen}>
-            <XStack className="gap-2.5 items-center justify-center">
-              <MarketIcon size={24} />
-              <Text className="text-base ">{t('assistants.market.title')}</Text>
-            </XStack>
-            <RowRightArrow />
-          </PressableRow>
+    <View style={{
+      flex:1,
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom
+    }}>
+      <YStack className='gap-2.5 flex-1'>
+          <YStack className="gap-1.5 px-2.5">
+            <PressableRow
+              className="flex-row justify-between items-center py-2.5 px-2.5 rounded-lg"
+              onPress={handleNavigateAssistantMarketScreen}>
+              <XStack className="gap-2.5 items-center justify-center">
+                <MarketIcon size={24} />
+                <Text className="text-base ">{t('assistants.market.title')}</Text>
+              </XStack>
+              <RowRightArrow />
+            </PressableRow>
 
-          <PressableRow
-            className="flex-row justify-between items-center py-2.5 px-2.5 rounded-lg"
-            onPress={handleNavigateAssistantScreen}>
-            <XStack className="gap-2.5 items-center justify-center">
-              <UnionIcon size={24} />
-              <Text className="text-base ">{t('assistants.market.my_assistant')}</Text>
-            </XStack>
-            <RowRightArrow />
-          </PressableRow>
-          <YStack className="px-2.5">
-            <Divider />
+            <PressableRow
+              className="flex-row justify-between items-center py-2.5 px-2.5 rounded-lg"
+              onPress={handleNavigateAssistantScreen}>
+              <XStack className="gap-2.5 items-center justify-center">
+                <UnionIcon size={24} />
+                <Text className="text-base ">{t('assistants.market.my_assistant')}</Text>
+              </XStack>
+              <RowRightArrow />
+            </PressableRow>
+            <YStack className="px-2.5">
+              <Divider />
+            </YStack>
           </YStack>
+
+          <MenuTabContent title={t('menu.topic.recent')} onSeeAllPress={handleNavigateTopicScreen}>
+            <YStack className="flex-1 min-h-[200px]">
+              {topics.length > 0 && (
+                <TopicList topics={topics} enableScroll={true} handleNavigateChatScreen={handleNavigateChatScreen} />
+              )}
+            </YStack>
+          </MenuTabContent>
         </YStack>
 
-        <MenuTabContent title={t('menu.topic.recent')} onSeeAllPress={handleNavigateTopicScreen}>
-          <YStack className="flex-1 min-h-[200px]">
-            {topics.length > 0 && (
-              <TopicList topics={topics} enableScroll={true} handleNavigateChatScreen={handleNavigateChatScreen} />
-            )}
-          </YStack>
-        </MenuTabContent>
-      </YStack>
+        <YStack className="px-5 pb-2.5">
+          <Divider />
+        </YStack>
 
-      <YStack className="px-5 pb-2.5">
-        <Divider />
-      </YStack>
-
-      <XStack className="justify-between items-center">
-        <PressableRow className="gap-2.5 items-center" onPress={handleNavigatePersonalScreen}>
-          <Image
-            className="w-12 h-12 rounded-full"
-            source={avatar ? { uri: avatar } : require('@/assets/images/favicon.png')}
-          />
-          <Text className="text-base">{userName || t('common.cherry_studio')}</Text>
-        </PressableRow>
-        <IconButton icon={<Settings size={24} />} onPress={handleNavigateSettingsScreen} style={{ paddingRight: 16 }} />
-      </XStack>
-    </SafeAreaContainer>
+        <XStack className="justify-between items-center">
+          <PressableRow className="gap-2.5 items-center" onPress={handleNavigatePersonalScreen}>
+            <Image
+              className="w-12 h-12 rounded-full"
+              source={avatar ? { uri: avatar } : require('@/assets/images/favicon.png')}
+            />
+            <Text className="text-base">{userName || t('common.cherry_studio')}</Text>
+          </PressableRow>
+          <IconButton icon={<Settings size={24} />} onPress={handleNavigateSettingsScreen} style={{ paddingRight: 16 }} />
+        </XStack>
+      </View>
   )
 }
