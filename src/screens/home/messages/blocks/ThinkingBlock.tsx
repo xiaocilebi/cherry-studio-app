598c2b0199d13bd4d4316e26dfbf150faa1e4d0b
eeee0717 <chentao020717Work@outlook.com>
2025-09-23 16:01:28 +0800

    chore: optimize thinking block style

--- src/screens/home/messages/blocks/ThinkingBlock.tsx
+++ src/screens/home/messages/blocks/ThinkingBlock.tsx
@@ -3,49 +3,67 @@ import { Pressable, StyleSheet, View } from 'react-native'
 
 import { ThinkingMessageBlock } from '@/types/message'
 
-import ReactNativeMarkdown from '../../markdown/ReactNativeMarkdown'
+import { LinearGradient } from 'expo-linear-gradient'
 import { MarqueeComponent } from '@/componentsV2'
+import ReactNativeMarkdown from '../../markdown/ReactNativeMarkdown'
+import { useTheme } from '@/hooks/useTheme'
 
 interface Props {
   block: ThinkingMessageBlock
 }
 
 const ThinkingBlock: React.FC<Props> = ({ block }) => {
+  const {isDark} = useTheme()
   const [expanded, setExpanded] = useState(false)
 
   const toggleExpanded = () => {
     setExpanded(!expanded)
   }
 
+  const gradientColors = isDark
+    ? ['rgba(2,111,180,0.22)', 'rgba(9,125,149,0.1)', 'rgba(18,73,109,0.2)'] // 暗色主题
+    : ['rgba(146,231,255,0.5)', 'rgba(206,232,255,0.5)', 'rgba(206,232,255,0.5)'] // 亮色主题
+
+  const borderColor = isDark ? 'rgba(0,103,168,0.5)' : 'rgba(80,183,255,1)'
+
   return (
-    <View style={styles.container}>
-      <Pressable onPress={toggleExpanded}>
-        <MarqueeComponent block={block} expanded={expanded} />
-      </Pressable>
-      {expanded && (
-        <View style={styles.contentContainer}>
-          <ReactNativeMarkdown block={block} />
-        </View>
-      )}
+    <View style={[styles.wrapper, { borderColor }]}>
+      <LinearGradient
+        colors={gradientColors as any}
+        locations={[0, 0.61, 1]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <Pressable onPress={toggleExpanded} style={styles.headerPressable}>
+          <MarqueeComponent block={block} expanded={expanded} />
+        </Pressable>
+        {expanded && (
+          <View style={styles.contentContainer}>
+            <ReactNativeMarkdown block={block} />
+          </View>
+        )}
+      </LinearGradient>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    // TODO: 里面的文本不会自动撑开，当有文字时才会撑开，不知道为什么，这里目前先写死
+  wrapper: {
     width: '100%',
-    paddingVertical: 10,
-    paddingHorizontal: 15,
     borderRadius: 16,
-    borderWidth: 0.5,
-    borderColor: 'rgba(255, 255, 255, 0.1)',
-    backgroundColor: 'rgba(0, 185, 107, 0.15)',
-    justifyContent: 'center'
+    borderWidth: 0.7,
+    overflow: 'hidden'
+  },
+  headerPressable: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems:'center',
+    paddingHorizontal:16,
+    paddingVertical:10
   },
   contentContainer: {
-    paddingHorizontal: 0,
-    paddingVertical: 5,
+    paddingHorizontal:16,
+    paddingVertical:10,
     borderRadius: 8
   }
 })
