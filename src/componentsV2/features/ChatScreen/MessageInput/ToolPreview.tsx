0b65d964846d4725a7848180a04a7436dff109cd
eeee0717 <chentao020717Work@outlook.com>
2025-10-12 13:47:42 +0800

    chore: mcpserver style

--- src/componentsV2/features/ChatScreen/MessageInput/ToolPreview.tsx
+++ src/componentsV2/features/ChatScreen/MessageInput/ToolPreview.tsx
@@ -38,7 +38,7 @@ export const ToolPreview: React.FC<ToolPreviewProps> = ({ assistant, updateAssis
     <View>
       <XStack className="gap-2">
         {assistant.model && assistant.enableGenerateImage && (
-          <XStack className="gap-[5px] rounded-[48px] py-1 px-1 bg-green-10 dark:bg-green-dark-10 border-[0.5px] border-green-20 dark:border-green-dark-20 justify-between items-center">
+          <XStack className="gap-1 rounded-xl py-1 px-2 bg-green-10 dark:bg-green-dark-10 border-[0.5px] border-green-20 dark:border-green-dark-20 justify-between items-center">
             <Palette size={20} className="text-green-100 dark:text-green-dark-100" />
             <Pressable onPress={handleDisableGenerateImage}>
               <X size={20} className="text-green-100 dark:text-green-dark-100" />
@@ -46,7 +46,7 @@ export const ToolPreview: React.FC<ToolPreviewProps> = ({ assistant, updateAssis
           </XStack>
         )}
         {assistant.model && assistant.enableWebSearch && (
-          <XStack className="gap-[5px] rounded-[48px] py-1 px-1 border-[0.5px] border-green-20 dark:border-green-dark-20 bg-green-10 dark:bg-green-dark-10 justify-between items-center">
+          <XStack className="gap-1 rounded-xl py-1 px-2 border-[0.5px] border-green-20 dark:border-green-dark-20 bg-green-10 dark:bg-green-dark-10 justify-between items-center">
             <Globe size={20} className="text-green-100 dark:text-green-dark-100" />
             <Pressable onPress={handleDisableWebSearch}>
               <X size={20} className="text-green-100 dark:text-green-dark-100" />
